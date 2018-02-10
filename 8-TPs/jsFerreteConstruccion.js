aa21ef45890624d7dc022d14eb382719e3507cf7
NicolasOdlV <30671017+NicolasOdlV@users.noreply.github.com>
2018-02-10 18:15:10 -0300

    falta 3ej de construccion

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -3,14 +3,24 @@ A.	 Mostrar la cantidad de alambre a comprar  si se ingresara el largo y el anch
 B.	Mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
 */
-
+var ancho;
+var largo;
+var radio;
+var rectanguloAlambre;
+var circuloAlambre;
+var contrapiso;
 function Rectangulo () 
 {
-
+    ancho=parseInt(document.getElementById("Ancho").value);
+    largo=parseInt(document.getElementById("Largo").value);
+    rectanguloAlambre=parseInt(6*(ancho+largo));
+    alert("Necesita "+rectanguloAlambre+" metros de alambre");
 }
 function Circulo () 
 {
-	
+    radio=parseInt(document.getElementById("Radio").value);
+    circuloAlambre=parseInt(6*radio*Math.PI);
+    alert("Necesita "+circuloAlambre+" metros de alambre");
 }
 function Materiales () 
 {
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -3,15 +3,36 @@ A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+var precio1;
+var precio2;
+var precio3;
+var suma;
+var promedio;
+var precioFinal;
+
 function Sumar () 
 {
-	
+    precio1=parseInt(document.getElementById("PrecioUno").value); 
+    precio2=parseInt(document.getElementById("PrecioDos").value);
+    precio3=parseInt(document.getElementById("PrecioTres").value);
+    suma=parseInt(precio1+precio2+precio3);
+    alert("La suma entre ellos es: "+suma);
 }
-function Promedio () 
+function Promedio ()   
 {
-	
+    precio1=parseInt(document.getElementById("PrecioUno").value); 
+    precio2=parseInt(document.getElementById("PrecioDos").value);
+    precio3=parseInt(document.getElementById("PrecioTres").value);
+    suma=parseInt(precio1+precio2+precio3);
+    promedio=parseInt(suma/3);
+    alert("El promedio es: "+promedio);
 }
 function PrecioFinal () 
 {
-	
+    precio1=parseInt(document.getElementById("PrecioUno").value); 
+    precio2=parseInt(document.getElementById("PrecioDos").value);
+    precio3=parseInt(document.getElementById("PrecioTres").value);
+    suma=parseInt(precio1+precio2+precio3);
+    precioFinal=parseInt(suma*1.21);
+    alert("El precio final es: "+precioFinal);
 }
