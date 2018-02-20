e71c4c2d50143a259eb4333aa8bcb1361c1ee777
NicolasOdlV <30671017+NicolasOdlV@users.noreply.github.com>
2018-02-19 22:00:56 -0300

    hasta ej 5 resuelto

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -1,6 +1,10 @@
 function Mostrar()
 {
 
+var sexo = prompt("ingrese f ó m .");
+    while (sexo!=null&&(sexo!="f"&&sexo!="m")) {
+        sexo=prompt("Ingrese f (femenino) o m (masculino)");
+    }
 
 }
 //FIN DE LA FUNCIÓN
